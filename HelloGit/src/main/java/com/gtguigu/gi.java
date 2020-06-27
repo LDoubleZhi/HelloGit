61105707731c4ba28af53789704fe09c2a48e10d
LDoubleZhi <email@example.com>
2020-06-27 21:18:13 +0800

    idea git test

---
+++ HelloGit/src/main/java/com/gtguigu/gi.java
@@ -0,0 +1,12 @@
+package com.gtguigu;
+
+/**
+ * @author : liulinzhi
+ * @date: 2020/06/27/21:00
+ * @description:
+ */
+public class gi {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
